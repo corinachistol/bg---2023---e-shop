f3e8a3426da1572c369e142083caf9a8f6f92c1b
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-09-10 14:56:11 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,41 +1,103 @@
 import Fastify from 'fastify'
+import fs from 'node:fs'
 
-const fastity = Fastify( {
+const fastify = Fastify( {
     logger:true
 } )
 
 //console.log(fastity)
 
-fastity.register(import('@fastify/postgres'), {
+fastify.register(import('@fastify/postgres'), {
     connectionString: 'postgres://postgres:postsql123@localhost:5432/js_shop'
 })
 
 
-fastity.get('/', async (req, reply) => {
-    const client = await fastity.pg.connect()
-    // console.log(client)
-    let result = []
 
-    try {
-        const { rows } = await client.query(`SELECT * from products`)
-        //console.log(rows)
+async function itemRoutes() {
+    fastify.get('/', async (req, reply) => {
+        const client = await fastify.pg.connect()
+        // console.log(client)
         
-        return rows
-        
-    } catch (err) {
-        console.log(err)
-    } finally {
-        //Release the client immediately after query resolve or throw error
-        client.release()
-    }
+        try {
+            const { rows } = await client.query(`SELECT * from products`)
+            //console.log(rows)
             
-})
+            return rows
+            
+        } catch (err) {
+            console.log(err)
+        } finally {
+            //Release the client immediately after query resolve or throw error
+            client.release()
+        }
+                
+    })
+
+    fastify.get('/clients', async (req,reply) => {
+        const  client = await fastify.pg.connect()
+
+        try {
+            const { rows } = await client.query(`SELECT * from clients`)           
+            return rows
+            
+        } catch (err) {
+            console.log(err)
+        } finally {
+            //Release the client immediately after query resolve or throw error
+            client.release()
+        }
+    })
+
+    fastify.get('/clients/:id', async (req,reply) => {
+        const  client = await fastify.pg.connect()
+        
+
+        try {
+            const { rows } = await client.query(`SELECT * from clients WHERE id = ${req.params.id}`)   
+            // const  { id }  = await req.params 
+           
+            // const item = rows.find(row => row.id == id)   
+            return rows
+         
+        
+        } catch (err) {
+            console.log(err)
+        } finally {
+            //Release the client immediately after query resolve or throw error
+            client.release()
+        }
+    })
+
+    fastify.post('/clients/add', async (req,reply) => {
+        const newClient = req.body
+        console.log(JSON.parse(newClient))
+        
+       
+        const  client = await fastify.pg.connect()
+
+        try {
+            // const { rows } = await client.query(`SELECT * from clients`)   
+            const response = await client.query(`INSERT INTO clients(name,address,phone,email,password) VALUES('${newClient.name}', '${newClient.address}','${newClient.phone}', '${newClient.email}','${newClient.pasword}') RETURNING id`)   
+            console.log(response)
+            return response         
+        
+        } catch (err) {
+            console.log(err)
+        } finally {
+            client.release()
+        }
+    })
+
+  
+}
+
+itemRoutes()
 
 const start = async () => {
     try {
-        await fastity.listen({ port: 3000 })
+        await fastify.listen({ port: 3000 })
     } catch (err) {
-        fastity.log.error(err)
+        fastify.log.error(err)
         process.exit(1)
         
     }
