2d1dcb12e6d18f8d9a353ac6dba60502bba1f9e0
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-09-12 14:54:33 +0300

    CRUD operations added

--- server-express.js
+++ server-express.js
@@ -1,13 +1,22 @@
 import express from "express";
+// import bodyParser from 'body-parser'
 import pg from 'pg';
+// const pool = new pg.Pool()
 
 const app = express();
 
+app.use(express.json())
+// app.use(bodyParser.urlencoded({extended:true}))
+
+const client = new pg.Client('postgres://postgres:postsql123@localhost:5432/js_shop')
+
 app.listen(3000)
 
-app.get('/', (req,res) => {
 
-    const client = new pg.Client('postgres://postgres:postsql123@localhost:5432/js_shop')
+app.get('/', (req,res) => {
+    // const client = new pg.Client('postgres://postgres:postsql123@localhost:5432/js_shop')
+
+    // const client = new pg.Client('postgres://postgres:postsql123@localhost:5432/js_shop')
 //     const client = new pg.Client({
 //     host:'localhost',
 //     port:'5432',
@@ -33,6 +42,113 @@ app.get('/', (req,res) => {
     } )
     
 })
+
+app.get('/clients', (req,res) => {
+    // const client = new pg.Client('postgres://postgres:postsql123@localhost:5432/js_shop')
+    client.connect( err=> {
+        if(err){
+            throw err
+        }
+
+        client.query(`Select * FROM clients`, (err,data) => {
+            if (err) {
+                throw err
+            }
+            console.log('>>> Table displayed')
+            res.send(data.rows)
+    
+            client.end()
+        })
+    } )
+})
+
+app.get('/clients/:id', (req,res) => {
+    const client = new pg.Client('postgres://postgres:postsql123@localhost:5432/js_shop')
+    client.connect( err=> {
+        if(err){
+            throw err
+        }
+        const id = req.params
+        client.query(`Select * FROM clients WHERE id = ${id}`, (err,data) => {
+            if (err) {
+                throw err
+            }
+            console.log('>>> Client displayed')
+            res.send(data.rows)
+    
+            client.end()
+        })
+    } )
+} )
+
+app.post('/clients/add', (req,res) => {
+    const client = new pg.Client('postgres://postgres:postsql123@localhost:5432/js_shop')
+    client.connect( err=> {
+        if(err){
+            throw err
+        }
+        const {name, address, phone, email, password} = req.body
+        
+        client.query(`INSERT INTO clients(name,address,phone,email,password) VALUES('${name}', '${address}','${phone}', '${email}','${password}') `, (err,data) => {
+            if (err) {
+                throw err
+            }
+            console.log('>>> Client added')
+            console.log(data)
+            res.send(data)
+    
+            client.end()
+        })
+    } )
+})
+
+
+
+app.patch('/clients/:id', (req,res) => {
+    const client = new pg.Client('postgres://postgres:postsql123@localhost:5432/js_shop')
+    client.connect( err=> {
+        if(err){
+            throw err
+        }
+        const newClient = req.body
+
+        const oldUserReq = client.query(`SELECT * FROM clients WHERE id = ${req.params.id} `)
+        const oldUser = oldUserReq.rows
+        console.log(oldUser)
+        
+        client.query(`UPDATE clients SET(name ,address , phone , email ,password) = ('${newClient.name}', '${newClient.address}','${newClient.phone}', '${newClient.email}','${newClient.password}') WHERE id  = ${req.params.id}`, (err,data) => {
+            if (err) {
+                throw err
+            }
+            console.log('>>> Client changed')
+            console.log(data)
+            res.send(data)
+    
+            client.end()
+        })
+    } )
+})
+
+app.delete('/clients/:id', (req,res) => {
+    const client = new pg.Client('postgres://postgres:postsql123@localhost:5432/js_shop')
+    client.connect( err=> {
+        if(err){
+            throw err
+        }
+        const id = req.params
+        client.query(`DELETE from clients WHERE id = ${req.params.id}`, (err,data) => {
+            if (err) {
+                throw err
+            }
+            console.log('>>> Client deleted')
+            res.send(data.rows)
+    
+            client.end()
+        })
+    } )
+} )
+
+
 
  
 
