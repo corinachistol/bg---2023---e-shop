28ec62f7d59f99aadc0b7ce817f537a71c42421a
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-09-14 22:59:39 +0300

    Add files via upload

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,36 @@
 import {getAllProducts, getAllClients, getClientById, addClient, updateClient, deleteClient } from '../controller/controller.js'
 
-export async function itemRoutes(fastify,options) {
+//schema for post one item
+const postClient = {
+    schema: {
+        body: {
+            type: 'object',
+            properties: {
+                name:{ type: 'string'},
+                address:{ type: 'string'},
+                phone:{ type: 'number'},
+                email:{ type: 'string'},
+                password:{ type: 'string'}
+            },
+            required: ['name', 'address', 'email','password']
+        }
+    }
+}
+
+
+export async function itemRoutes(fastify,options, done) {
     fastify.get('/', getAllProducts)
 
     fastify.get('/clients', getAllClients)
 
     fastify.get('/clients/:id', getClientById )
 
-    fastify.post('/clients/add', addClient )
+    fastify.post('/clients/add',postClient, addClient )
 
-    fastify.patch('/clients/:id', updateClient)
+    fastify.patch('/clients/:id',postClient, updateClient)
 
     fastify.delete('/clients/:id', deleteClient)
+
+    done()
  
 }
