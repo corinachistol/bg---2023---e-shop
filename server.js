1c6f566919ece6b25e64b8d2c72f0d51c3122378
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-09-14 14:14:20 +0300

    Fixed the bug "Internal server error"

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 import Fastify from 'fastify'
 import { itemRoutes } from './routes/routes.js'
 
+
 const fastify = Fastify( {
     logger:true
 } )
 
 //console.log(fastity)
-fastify.register(itemRoutes)
 
 fastify.register(import('@fastify/postgres'), {
     connectionString: 'postgres://postgres:postsql123@localhost:5432/js_shop'
+    
 })
 
-// const client = await fastify.pg.connect()
+fastify.register(itemRoutes)
 
-// itemRoutes(fastify)
 
 const start = async () => {
     try {
         await fastify.listen({ port: 3000 })
+        fastify.log.info(`Server is listening on port:3000`)
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
