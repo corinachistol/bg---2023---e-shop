f022864f4f6529e349ff0e9fdec634bebd447535
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-09-18 13:21:55 +0300

    Add files via upload

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,42 @@
-import {getAllProducts, getAllClients, getClientById, addClient, updateClient, deleteClient } from '../controller/controller.js'
+import fastify from 'fastify'
+import {getAllProducts, getAllClients, getClientById, addClient, updateClient, deleteClient, getHomePage } from '../controller/controller.js'
+
+
+const getAllClientsOps = {
+    schema:{
+        response: {
+            200: {
+                type: 'array',
+                items: {
+                    type: 'object',
+                    properties: {
+                        id: {type:'string'},
+                        name: {type: 'string'}
+                    }
+                } 
+            }
+        }
+    }
+}
+
+//nu functioneaza
+const getClientOps = {
+    schema:{
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    id: {type:'string'},
+                    name: {type:'string'},
+                }
+            }
+        }
+    }
+}
+
 
 //schema for post one item
-const postClient = {
+const postClientOps = {
     schema: {
         body: {
             type: 'object',
@@ -18,19 +53,27 @@ const postClient = {
 }
 
 
-export async function itemRoutes(fastify,options, done) {
-    fastify.get('/', getAllProducts)
+export async function clientRoutes(fastify,options) {
 
-    fastify.get('/clients', getAllClients)
+    fastify.get('/',getAllClientsOps, getAllClients)
 
-    fastify.get('/clients/:id', getClientById )
+    fastify.get('/:id', getClientById )
 
-    fastify.post('/clients/add',postClient, addClient )
+    fastify.post('/add',postClientOps, addClient )
 
-    fastify.patch('/clients/:id',postClient, updateClient)
+    fastify.patch('/:id',postClientOps, updateClient)
 
-    fastify.delete('/clients/:id', deleteClient)
-
-    done()
+    fastify.delete('/:id', deleteClient)
  
 }
+
+export async function productRoute (fastify,option) {
+
+    fastify.get('/', getAllProducts)
+}
+
+export async function homePage(fastify,options){
+    fastify.get('/', getHomePage)
+}
+
+
