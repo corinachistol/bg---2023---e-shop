4f318126d94c95b5c9aa8a322d4b77606ca6bc3b
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-10-02 14:51:50 +0300

    Add files via upload

---
+++ public/src/server.js
@@ -0,0 +1,21 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import Fastify from 'fastify';
+const fastify = Fastify();
+fastify.get('/', (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
+    reply.code(200).send({ status: "Active" });
+}));
+fastify.listen({ port: 3000 }, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server is listening at ${address}`);
+});
