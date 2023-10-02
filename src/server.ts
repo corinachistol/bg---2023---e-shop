ddd60a9b948a5d6b8117914de4e7882d7f98c596
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-10-02 14:58:34 +0300

    Add files via upload

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,15 @@ fastify.get('/', async (request, reply) => {
     reply.code(200).send({status:"Active"}) 
 })
 
-fastify.listen({port: 3000}, (err,address)=> {
-    if(err) {
-        console.error(err)
+const start = async () => {
+    try {
+        await fastify.listen({ port: 3000 })
+        fastify.log.info(`Server is listening on port:3000`)
+    } catch (err) {
+        fastify.log.error(err)
         process.exit(1)
+        
     }
-    console.log(`Server is listening at ${address}`)
-})
+}
+
+start()
