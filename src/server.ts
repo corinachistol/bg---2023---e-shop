af0fa32d723eafc69a2878123c9be3fe18231e9d
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-10-02 22:11:45 +0300

    #6 added pg server connection

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,15 @@ import Fastify from 'fastify';
 
 const fastify = Fastify()
 
+fastify.register(import('fastify-typeorm-plugin'), {
+    type: 'postgres',
+    host:'localhost',
+    port: 3000,
+    username: 'postgres',
+    password: 'postsql123',
+    database: './e_shop_ff_orm.psql'
+})
+
 fastify.get('/', async (request, reply) => {
     reply.code(200).send({status:"Active"}) 
 })
