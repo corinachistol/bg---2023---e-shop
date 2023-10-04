2d18ad0c837777d0b4b4b2d9df6897ccf74a65fa
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-10-04 14:39:39 +0300

    fixed connection

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,8 @@ const fastify = Fastify({logger:true})
 fastify.register(import('fastify-typeorm-plugin'),{
     type: 'postgres',
     host:'localhost',
-    port: 3000,
-    database: './e_shop_ff_orm.psql',
+    port: 5432,
+    database: 'e_shop_ff_orm',
     username: 'postgres',
     password: 'postsql123',
     logging: true,
@@ -15,35 +15,28 @@ fastify.register(import('fastify-typeorm-plugin'),{
     entities: [Product],
 })
 
+
 fastify.get('/', async (request, reply) => {
     console.log("Active")
-    // const products = await this.orm
-    //     .getRepository(Product)
-    //     .createQueryBuilder('products')
-    //     .getMany()
+
+    const products = await fastify.orm
+        .getRepository(Product)
+        .createQueryBuilder('products')
+        .getMany()
     
-    //     return products;
-    reply.code(200).send({status:"Active"}) 
+        return products;
+    // reply.code(200).send({status:"Active"}) 
 })
 
-fastify.listen({ port: 8080 }, (err, address) => {
-    if (err) {
-      console.error(err)
-      process.exit(1)
-    }
-    console.log(`Server listening at ${address}`)
-  })
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 })
+    fastify.log.info(`Server is listening on port:3000`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+    
+  }
+}
 
-
-//   const start = async () => {
-//     try {
-//         await fastify.listen({ port: 3000 })
-//         fastify.log.info(`Server is listening on port:3000`)
-//     } catch (err) {
-//         fastify.log.error(err)
-//         process.exit(1)
-        
-//     }
-// }
-
-// start()
+start()
