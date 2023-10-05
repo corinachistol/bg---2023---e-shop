b398b901a05ccb2ee24512bbe326790f49bd1e56
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-10-05 13:16:39 +0300

    not done yet

--- src/product/api.ts
+++ src/product/api.ts
@@ -6,8 +6,28 @@ const fastify = Fastify({logger:true})
 
 export async function postRoute (fastify:FastifyInstance, options: object) {
     fastify.post('/products', async (request, reply) => {
-        console.log(request.body)
-        const newProduct = new Product()
+        try {
+            const newData = request.body
+            console.log(newData)
+
+            const newItem = await fastify.orm.getRepository(Product).create(newData)
+            await 
+
+            // const query = await fastify.orm.createQueryBuilder().insert().into(Product).values({
+            //     name: name,
+            //     priceAmount: priceAmount,
+            //     priceCurrency: priceCurrency
+
+            // }).execute()
+            console.log('>>>>> item added')
+                                 
+            reply.code(201).send(newItem)
+
+        } catch (error) {
+            reply.code(500).send({error: 'Error adding a new product!'})
+        }
+        
+        // await userRepository.save(newProduct)
         
 
     })
