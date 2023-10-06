09588617b869cd7d9420efef8018f7dace771ac7
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-10-06 22:22:10 +0300

    postClient function

--- src/client/api.ts
+++ src/client/api.ts
@@ -1,12 +1,12 @@
 
-import Fastify, { FastifyInstance } from "fastify";
+import Fastify, { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { Client } from "./entities.js";
 
 
 const fastify: FastifyInstance = Fastify({logger:true})
 
-export async function getAllClients(fastify,options){
-    fastify.get('/clients', async (request, reply) => {
+export async function getAllClients(fastify:FastifyInstance,options){
+    fastify.get('/clients', async (request:FastifyRequest, reply:FastifyReply) => {
            const clients = await fastify.orm
                .getRepository(Client)
                .createQueryBuilder('clients')
@@ -15,6 +15,34 @@ export async function getAllClients(fastify,options){
            reply.code(200).send({clients})
     })
 
+}
+
+export async function postClient(fastify:FastifyInstance,options){
+    fastify.route({
+        method: "POST",
+        url: "/clients/add",
+        handler:async (request:FastifyRequest, reply:FastifyReply) => {
+            try {
+                const { first_name, last_name, phone, email,address, password } = request.body
+                const newClient = await fastify.orm
+                    .createQueryBuilder()
+                    .insert()
+                    .into(Client)
+                    .values([{
+                        nameFirst: first_name,
+                        nameLast:last_name,
+                        phone:phone,
+                        email:email,
+                        address: address,
+                        password:password
+                    }]).execute()
+                return reply.code(21).send({newClient})
+            } catch (error) {
+                
+            }              
+        }
+    })
+    
 }
 
 interface IQueryInterface{
@@ -32,8 +60,8 @@ interface IReply {
     body:any;
 }
 
-fastify.get<{Querystring: IQueryInterface, Headers:IHeaders, Reply: IReply}>('/clients', async (reques,reply)=>{
-    const { username,password } = reques.body
+fastify.get<{Querystring: IQueryInterface, Headers:IHeaders, Reply: IReply}>('/clients', async (request,reply)=>{
+    const { username,password } = request.body
     return reply.send({
         code:200,
         message: "success",
