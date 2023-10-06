396aa354d431f4a681cee1868c531d735b111ec7
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-10-06 15:16:25 +0300

    Add files via upload

--- src/client/api.ts
+++ src/client/api.ts
@@ -1,8 +1,9 @@
 
-import Fastify from "fastify";
+import Fastify, { FastifyInstance } from "fastify";
 import { Client } from "./entities.js";
 
-const fastify = Fastify({logger:true})
+
+const fastify: FastifyInstance = Fastify({logger:true})
 
 export async function getAllClients(fastify,options){
     fastify.get('/clients', async (request, reply) => {
@@ -15,6 +16,32 @@ export async function getAllClients(fastify,options){
     })
 
 }
+
+interface IQueryInterface{
+    username:string;
+    password:string
+}
+
+interface IHeaders{
+    'x-access-token': string;
+}
+
+interface IReply {
+    code:number;
+    message: string;
+    body:any;
+}
+
+fastify.get<{Querystring: IQueryInterface, Headers:IHeaders, Reply: IReply}>('/clients', async (reques,reply)=>{
+    const { username,password } = reques.body
+    return reply.send({
+        code:200,
+        message: "success",
+        body: {
+            username,password
+        }
+    })
+})
 
 
 
