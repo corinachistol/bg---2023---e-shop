66d57bc6a90e0b471cad5bebcabd222bae1b220d
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-10-06 10:41:11 +0300

    added fastify.route()

--- src/product/api.ts
+++ src/product/api.ts
@@ -1,34 +1,82 @@
 //route post
-import Fastify, { FastifyInstance } from 'fastify'
+import Fastify, { FastifyInstance,FastifySchema } from 'fastify'
 import { Product } from './entities.js'
+import { postProductSchema } from './schema.js'
 
 const fastify = Fastify({logger:true})
 
 export async function postRoute (fastify:FastifyInstance, options: object) {
-    fastify.post('/products', async (request, reply) => {
-        try {
-            const newData = request.body
-            console.log(newData.name, newData.priceAmount,newData.priceCurrency)
 
-            const newProduct = await fastify.orm
-                .createQueryBuilder()
-                .insert()
-                .into(Product)
-                .values({
-                    name: newData.name,
-                    price: `${newData.priceAmount}:${newData.priceCurrency}`,
-                    // priceCurrency: newData.priceCurrency
-
-            }).execute()
-            console.log('>>>>> item added')
-                                 
-            reply.code(201).send(newProduct)
-
-        } catch (error) {
-            reply.code(500).send({error: 'Error adding a new product!'})
-        }      
-
+    fastify.route({
+        method: "POST",
+        url: '/products',
+        schema:postProductSchema,
+        handler: async (request, reply) => {
+            // let newData = {name: string, priceAmount: integer, priceCurrency: string}
+            try {
+                const {name, priceAmount, priceCurrency} = request.body
+                // const newProduct = await fastify.orm.manager.create(Product, {
+                //     name: name,
+                //     priceAmount: priceAmount,
+                //     priceCurrency: priceCurrency
+                // })
+    
+                 
+                
+    
+                const newProduct = await fastify.orm
+                    .createQueryBuilder()
+                    .insert()
+                    .into(Product)
+                    .values({
+                        name: name,
+                        priceAmount: priceAmount,
+                        priceCurrency: priceCurrency
+    
+                }).execute()
+                console.log('>>>>> item added')
+                                     
+                reply.code(201).send(newProduct)
+    
+            } catch (error) {
+                reply.code(500).send({error: 'Error adding a new product!'})
+            }      
+    
+        }
     })
+
+    // fastify.post('/products',postProductSchema, async (request, reply) => {
+    //     // let newData = {name: string, priceAmount: integer, priceCurrency: string}
+    //     try {
+    //         const {name, priceAmount, priceCurrency} = request.body
+    //         // const newProduct = await fastify.orm.manager.create(Product, {
+    //         //     name: name,
+    //         //     priceAmount: priceAmount,
+    //         //     priceCurrency: priceCurrency
+    //         // })
+
+             
+            
+
+    //         const newProduct = await fastify.orm
+    //             .createQueryBuilder()
+    //             .insert()
+    //             .into(Product)
+    //             .values({
+    //                 name: name,
+    //                 priceAmount: priceAmount,
+    //                 priceCurrency: priceCurrency
+
+    //         }).execute()
+    //         console.log('>>>>> item added')
+                                 
+    //         reply.code(201).send(newProduct)
+
+    //     } catch (error) {
+    //         reply.code(500).send({error: 'Error adding a new product!'})
+    //     }      
+
+    // })
 
 }
 
