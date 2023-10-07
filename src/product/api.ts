a3bd231d02d7dfb7a0cda9de3eacc0ee12c407aa
corinachistol <115491975+corinachistol@users.noreply.github.com>
2023-10-07 14:45:46 +0300

    Add files via upload

--- src/product/api.ts
+++ src/product/api.ts
@@ -5,30 +5,43 @@ import { postProductSchema } from './schema.js'
 
 const fastify: FastifyInstance = Fastify({logger:true})
 
-export async function postRoute (fastify, options: object) {
+export async function postRoute (fastify:FastifyInstance, options: object) {
 
     fastify.route({
         method: "POST",
         url: '/products',
-        schema:postProductSchema,
-        handler: async (request:FastifyRequest, reply:FastifyReply) => {
+        // schema:postProductSchema,
+        handler: async (request:FastifyRequest, reply:FastifyReply ) => {
             try {
-                const {id, name, amount, currency} = request.body
-                  
-                const newProduct = await fastify.orm
-                    .createQueryBuilder()
-                    .insert()
-                    .into(Product)
-                    .values({
-                        // products_id: id,
-                        products_name: name,
-                        products_priceAmount: amount,
-                        products_priceCurrency: currency
-    
-                }).execute()
+                // const {id, name, amount, currency} = request.body
+                // console.log(name, amount, currency)
+
+                const createQuery = await fastify.orm.createQueryBuilder()
+                console.log('1.create Query', createQuery)
+
+                const insert = createQuery.insert().into(Product)
+                console.log('2.insert into', insert)
+
+                const values = insert.values({
+                    name: "Samsung Galaxy s20",
+                    priceAmount: 800,
+                    priceCurrency: "USD"
+                })
+                console.log('3.values', values)
+
+                const execute = values.execute()
+                console.log('4.execute',execute)
+
+                //o alta varianta
+                // const product = await fastify.orm.getRepository(Product).create(request.body)
+                // console.log('1.product', product)
+                // const results =  await fastify.orm.getRepository(Product).save(product)
+                // console.log('2 results', results)
+                
+                    
                 console.log('>>>>> item added')
                                      
-                reply.code(201).send(newProduct)
+                reply.code(201).send(execute)
     
             } catch (error) {
                 reply.code(500).send({error: 'Error adding a new product!'})
